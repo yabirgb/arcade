0db6164ae3e6c1e449b26ad0cc597c9dc2c41eea
Yabir <yabirgb@gmail.com>
2019-10-11 23:08:42 +0200

    Fixs + black

--- arcade/utils.py
+++ arcade/utils.py
@@ -3,13 +3,12 @@ import os
 import yaml
 
 
-def list_content(base_folder:str, contents:str='contents') -> List[str]:
-    
+def list_content(base_folder: str, contents: str = "contents") -> List[str]:
+
     path = os.path.join(base_folder, contents)
 
-
     contents = []
-    
+
     for root, folders, files in os.walk(path):
 
         for filen in files:
@@ -17,41 +16,43 @@ def list_content(base_folder:str, contents:str='contents') -> List[str]:
 
     return contents
 
-def generate_folder_structure(base_path:str) -> None:
+
+def generate_folder_structure(base_path: str) -> None:
     """
     Create folder structure of arcade in the
     user folder structure
     """
+
     def create_folder(path):
         if not os.path.exists(path):
             os.makedirs(path)
 
-    required_folders = ['contents', 'static', 'public']
+    required_folders = ["contents", "static", "public"]
 
     for folder in required_folders:
         create_folder(os.path.join(base_path, folder))
 
-def create_config_file(base_path:str,
-                       config:Dict[str, Union[Dict, str, int]
-                       name:str='arcade.yaml') -> None:
 
-    {
-        page_name = config['page_name'],
-        base_path = '',
-        author_name = '',
-        social = {
-            twitter = None,
-            github = None,
-            email = None,
-            mastodon = None,
-            linkedin = None,
-        }
-        
+def create_config_file(
+    base_path: str, config: Dict[str, Union[Dict, str, int]], name: str = "arcade.yaml"
+) -> None:
+
+    config = {
+        page_name: config["page_name"],
+        base_path: "",
+        author_name: "",
+        social: {
+            twitter: None,
+            github: None,
+            email: None,
+            mastodon: None,
+            linkedin: None,
+        },
     }
-    
+
     # Convert configuration to yaml format
     config_yaml = yaml.dump(config)
 
     # Write to disc the configuration file
-    with open(os.path.join(base_path,name), 'w') as f:
+    with open(os.path.join(base_path, name), "w") as f:
         f.write(config_yaml)
